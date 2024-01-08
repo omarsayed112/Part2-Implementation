5cf912f5f031e05eedc9879199ca82c769566f1e
omarsayed112 <os521260@gmail.com>
2024-01-08 22:23:55 +0200

    I have implemented the GUI class CompetitorGUI and cusomized it to do different things as you can see when the program is ran. And completed all the documentation of the code with comments as well as tried to customize it to be the best visually as much as I can.

--- Manager.java
+++ Manager.java
@@ -1,8 +1,14 @@
+import javax.swing.*;
+
 public class Manager {
     public static void main(String[] args) {
         CompetitorList competitorList = new CompetitorList();
         competitorList.readCompetitorsFromCSV("RunCompetitor.csv");
+        SwingUtilities.invokeLater(() -> {
+            CompetitorGUI competitorGUI = new CompetitorGUI(competitorList);
+        });
         competitorList.printShortDetails(100);
         competitorList.generateFinalReport();
     }
 }
+
